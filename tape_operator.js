1aabf7ec379f17cae3ebee3e46d0c0b74d666a1e
Vyachikk <112702422+Vyachikk@users.noreply.github.com>
2026-01-31 13:52:16 +0300

    Add files via upload

--- tape_operator.js
+++ tape_operator.js
@@ -6,57 +6,102 @@
 (function() {
     'use strict';
 
-    // Ждем загрузки Lampa
-    function waitForLampa(callback) {
-        if (window.Lampa) {
-            callback();
-        } else {
-            setTimeout(() => waitForLampa(callback), 100);
+    // Создаем заглушку компонента
+    const component = {
+        template: `<div style="display:none"></div>`,
+        mounted() {
+            // Пустая функция
         }
+    };
+
+    // Функция для перевода (заглушка)
+    function translate(text) {
+        // Просто возвращаем текст как есть
+        return text;
     }
 
-    waitForLampa(function() {
-        console.log('Минимальная кнопка загружена');
+    // Основная функция инициализации
+    function initMain() {
+        console.log('Минимальная кнопка запущена');
         
-        // HTML кнопки
+        // Регистрируем компонент (заглушка)
+        if (Lampa.Component && Lampa.Component.add) {
+            Lampa.Component.add('minimal_button', component);
+        }
+
+        // Манифест плагина (упрощенный)
+        const manifest = {
+            type: 'video',
+            version: '1.0',
+            name: 'Минимальная кнопка - 1.0',
+            description: 'Простая тестовая кнопка',
+            component: 'minimal_button'
+        };
+
+        // Регистрируем манифест
+        if (Lampa.Manifest) {
+            Lampa.Manifest.plugins = manifest;
+        }
+
+        // HTML кнопки (упрощенный SVG)
         const buttonHTML = `
-            <div class="full-start__button selector view--minimal_button">
+            <div class="full-start__button selector view--minimal_button" data-subtitle="minimal_button 1.0">
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
-                    <circle cx="12" cy="12" r="10"/>
-                    <polygon points="10,8 16,12 10,16"/>
+                    <path d="M8 5v14l11-7z"/>
                 </svg>
                 <span>Тест</span>
             </div>
         `;
 
-        // Добавляем кнопку на страницу карточки
-        Lampa.Listener.follow('full', function(e) {
-            if (e.type === 'complite') {
-                // Создаем кнопку
-                const btn = $(buttonHTML);
-                
-                // Простой обработчик клика
-                btn.on('hover:enter', function() {
-                    // Показываем уведомление
-                    Lampa.Noty.show('Кнопка работает!');
+        // Добавляем кнопку при открытии карточки фильма
+        if (Lampa.Listener) {
+            Lampa.Listener.follow('full', function(e) {
+                if (e.type === 'complite') {
+                    // Создаем элемент кнопки
+                    const btn = $(buttonHTML);
                     
-                    // Выводим в консоль
-                    console.log('Простая кнопка нажата');
+                    // Простой обработчик событий
+                    btn.on('hover:enter', function() {
+                        // Показываем простое уведомление
+                        if (Lampa.Noty && Lampa.Noty.show) {
+                            Lampa.Noty.show('Тестовая кнопка нажата!');
+                        } else {
+                            console.log('Тестовая кнопка нажата!');
+                        }
+                    });
                     
-                    // Получаем информацию о фильме
-                    if (e.data && e.data.movie) {
-                        const title = e.data.movie.title || 'Неизвестно';
-                        console.log('Название фильма:', title);
+                    // Добавляем кнопку после кнопки torrent
+                    const torrentBtn = e.object.activity.render().find('.view--torrent');
+                    if (torrentBtn.length) {
+                        torrentBtn.after(btn);
+                    } else {
+                        // Если кнопки torrent нет, добавляем в начало
+                        const buttonsContainer = e.object.activity.render().find('.full-start__buttons');
+                        if (buttonsContainer.length) {
+                            buttonsContainer.prepend(btn);
+                        }
                     }
-                });
-                
-                // Ищем, куда вставить кнопку
-                const container = e.object.activity.render().find('.full-start__buttons');
-                if (container.length) {
-                    container.append(btn);
                 }
-            }
-        });
-    });
+            });
+        }
+    }
+
+    // Запускаем плагин при загрузке
+    function startPlugin() {
+        if (window.Lampa) {
+            initMain();
+        } else {
+            // Ждем загрузки Lampa
+            const checkLampa = setInterval(function() {
+                if (window.Lampa) {
+                    clearInterval(checkLampa);
+                    initMain();
+                }
+            }, 100);
+        }
+    }
+
+    // Запускаем плагин
+    startPlugin();
 
 })();
